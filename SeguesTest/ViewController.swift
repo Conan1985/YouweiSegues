3eb7bcad0358452cb51609cee181735bc6228fe4
Conan1985 <Conan1985@users.noreply.github.com>
2018-07-20 17:11:24 -0400

    Enable data pass between views

--- SeguesTest/ViewController.swift
+++ SeguesTest/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func GoNext(_ sender: Any) {
+        print("clicked the button in first screen")
+        performSegue(withIdentifier: "firstSegue", sender: self)
+    }
+    
+    var username:String = ""
+    
+    @IBOutlet weak var usernameLabel:UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        usernameLabel?.text = username
     }
 
     override func didReceiveMemoryWarning() {
